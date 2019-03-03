294ae8b9d82b27bc3d5ca0248afc79ee398f2e0e
Shao Hui <shinlos@hotmail.com>
2019-03-04 00:02:42 +0800

    fix spelling error

--- src/11.spec.ts
+++ src/11.spec.ts
@@ -8,7 +8,7 @@ const list_1 = [4, 2, 5, 2, 4, 1, 8, 11, 2]
 // const list_4 = [1, [2, [3, 4, [5], 6]], 7, [8, [9]]]
 const list_5 = [1, 1, 1, 2, 3, 4, 4, 4, 3, 2, 2, 1, 2]
 
-describe('encode list_1', () => {
+describe('encodeModified list_1', () => {
 
   it('should return [4, 2, 5, 2, 4, 1, 8, 11, 2]', () => {
     for (let f of array_of_functions) {
@@ -19,7 +19,7 @@ describe('encode list_1', () => {
 
 })
 
-describe('encode list_5', () => {
+describe('encodeModified list_5', () => {
 
   it('should return [[3, 1], 2, 3, [3, 4], 3, [2, 2], 1, 2]', () => {
     for (let f of array_of_functions) {
