3864c9c82261a8848c29aa984d49a47b83058e31
Shao Hui <shinlos@hotmail.com>
2019-03-03 21:42:05 +0800

    fix spelling error

--- src/10.spec.ts
+++ src/10.spec.ts
@@ -19,7 +19,7 @@ describe('encode list_1', () => {
 
 })
 
-describe('encode list_6', () => {
+describe('encode list_5', () => {
 
   it('should return [[3, 1], [1, 2], [1, 3], [3, 4], [1, 3], [2, 2], [1, 1], [1, 2]]', () => {
     for (let f of array_of_functions) {
